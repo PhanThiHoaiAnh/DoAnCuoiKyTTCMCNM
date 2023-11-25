961e98cd6e323ae606ab9e19497ea613f9cdd076
dat2072002 <duydp44@gmail.com>
2023-11-25 22:23:40 +0700

    add cart % detailpro

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Product;
+use App\Models\Cart;
 
 class HomeController extends Controller
 {
@@ -28,6 +29,61 @@ class HomeController extends Controller
         {
             $product=Product::paginate(6);
         return view('home.userpage', compact('product'));
+        }
+    }
+
+    public function product_details($id)
+    {
+        $product=product::find($id);
+        return view('home.product_details',compact('product'));
+    }
+
+    public function add_cart(Request $request,$id)
+    {
+        if(Auth::id())
+        {
+            $user=Auth::user();
+            
+            $product=product::find($id);
+
+            $cart=new cart;
+
+            $cart->name=$user->name;
+
+            $cart->email=$user->email;
+
+            $cart->phone=$user->phone;
+
+            $cart->address=$user->address;
+
+            $cart->user_id=$user->id;
+            
+            $cart->Product_title=$product->title;
+
+            if($product->discount_price!=null)
+            {
+                $cart->price=$product->discount_price * $request->quantity;
+            }
+            else
+            {
+                $cart->price=$product->price * $request->quantity;
+            }
+
+            $cart->image=$product->image;
+
+            $cart->Product_id=$product->id;
+
+            $cart->quantity=$request->quantity;
+
+            $cart->save();
+
+            return redirect()->back();
+            
+        }
+
+        else
+        {
+            return redirect('login');
         }
     }
 }
