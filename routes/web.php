4ed624bcd0d25f365f8f5f2bc635ea70ddc90c15
dat2072002 <duydp44@gmail.com>
2023-11-26 16:24:46 +0700

    pay

--- routes/web.php
+++ routes/web.php
@@ -58,4 +58,10 @@ route::get('/remove_cart/{id}',[HomeController::class,'remove_cart']);
 
 route::get('/cash_order',[HomeController::class,'cash_order']);
 
+<<<<<<< HEAD
 route::get('/stripe/{totalprice}',[HomeController::class,'stripe']);
+=======
+route::get('/stripe/{totalprice}',[HomeController::class,'stripe']);
+
+Route::post('stripe/{totalprice}',[HomeController::class,'stripePost'])->name('stripe.post');
+>>>>>>> 5601ec4 (add payment)
